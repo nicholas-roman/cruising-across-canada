7f2178793d193c0a422ec31441ad4bd1e6d759b1
Tabitha Wong <tabi159w@gmail.com>
2021-02-13 13:59:35 -0500

    added function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-sldkfjs
+function greet()
+{
+  var username = document.getElementById("username").value;
+  speech(username);
+  document.getElementById("header").innerHTML = "Hello " + username +"! Ready to start your road trip?";
+  document.getElementById("nameform").style.display="none";        
+  document.getElementById("button").style.display="visible";
+  document.getElementById("des").style.display="none";
+  document.getElementById("button").style.visibility="visible";
+  return false;
+}
