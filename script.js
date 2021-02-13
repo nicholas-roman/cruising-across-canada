489ec4b2c32db06f4c0a2d0bb5a57a99c476f4b4
Tabitha Wong <tabi159w@gmail.com>
2021-02-13 14:12:56 -0500

    added file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function greet()
 {
   var username = document.getElementById("username").value;
   speech(username);
-  document.getElementById("header").innerHTML = "Hello " + username +"! Ready to start your road trip?";
+  document.getElementById("greeting").innerHTML = "Hello " + username +"! Ready to start your road trip?";
   document.getElementById("nameform").style.display="none";        
   document.getElementById("button").style.display="visible";
   document.getElementById("des").style.display="none";
