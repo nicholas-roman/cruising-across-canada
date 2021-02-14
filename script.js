8079cdc7abf4b2547ac7c9b211d1a412e3a956c4
Tabitha Wong <tabi159w@gmail.com>
2021-02-13 20:49:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+storage = window.localStorage;
 var nameinput = document.getElementById("nameinput").value;
-var honey = 5;
+storage.setItem("name", nameinput);
+storage.setItem("honey", 5);
 
 function displayhoney() {
+    var honey = storage.getItem("honey");
     document.getElementById("honey").innerHTML = honey;
 }
 
 function losehoney() {
+    var honey = storage.getItem("honey");
     honey = honey - 1;
+    storage.setItem("honey", honey);
     if (honey == 0) {
         window.location.href = "fail.html";
     }
@@ -17,5 +22,6 @@ function losehoney() {
 }
 
 function displayname() {
+    var nameinput = storage.getItem("name");
     document.getElementById("name").innerHTML = nameinput;
 }
