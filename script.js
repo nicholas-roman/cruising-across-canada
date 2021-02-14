d31571c02a031d36a1f87ff5d035347647bc9377
Tabitha Wong <tabi159w@gmail.com>
2021-02-14 00:09:04 -0500

    bye bye honey

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 storage = window.sessionStorage;
 
 function starthoney() {
-    let honey = 4;
-    sessionStorage.setItem("honey", honey);
+    return false;
 }
 
 function displayhoney() {
-    let honey = sessionStorage.getItem("honey");
-    document.getElementById("honey").innerHTML = honey;
+    return false;
 }
 
 function losehoney() {
-    let honey = sessionStorage.getItem("honey");
-    honey = honey - 1;
-    sessionStorage.setItem("honey", honey);
-    if (honey == -1) {
-        window.location.replace("fail.html");
-    }
-    else {
-        window.alert("Not quite... Try again!");
-        displayhoney();
-    }
+    return false;
 }
 
 function resethoney() {
-    let honey = 0;
-    sessionStorage.setItem("honey", honey);
+    return false;
 }
