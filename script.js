8e328f03bb954fcdc13f0d787da99818a54770ea
Mabel Luo <mabel.luom@gmail.com>
2021-02-13 20:30:47 -0500

    Add JS

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function displayhoney() {
 
 function losthoney() {
     honey = honey - 1;
-    displayhoney()
+    displayhoney();
     document.getElementByClass("question").innerHTML = "Not quite... Try again";
 }
 
