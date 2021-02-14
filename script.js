b09677dd043efc7792a7bdffdba96c038cf6f293
Tabitha Wong <tabi159w@gmail.com>
2021-02-13 23:23:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ function displayhoney() {
 
 function losehoney() {
     var honey = sessionStorage.getItem("honey");
-    sessionStorage.setItem("honey", honey);
     honey = honey - 1;
+    sessionStorage.setItem("honey", honey);
+<<<<<<< HEAD
+    honey = honey - 1;
+=======
+>>>>>>> parent of efb9986 (Update script.js)
     if (honey == 0) {
         window.location.replace("fail.html");
     }
